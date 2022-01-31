a0f5415b599d5036f3e017792693735bc9a9cba4
Lance Wolfe <lwolfe@talentpath.com>
2022-01-31 16:56:00 -0500

    Allow certificates to have names

--- models/certificateModel.js
+++ models/certificateModel.js
@@ -6,6 +6,10 @@ module.exports = (sequelize, DataTypes) => {
       primaryKey: true,
       autoIncrement: true,
     },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
     initialAmount: {
       type: DataTypes.DECIMAL(19, 4),
       allowNull: false,
